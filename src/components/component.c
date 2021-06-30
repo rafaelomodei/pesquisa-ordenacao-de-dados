3aebae1ba5ce1146ce3281b757b554768e5e6e97
Rafael Omodei <rafael.omodei@dasa.com.br>
2021-06-29 23:44:21 -0300

    feat: creat menu

---
+++ src/components/component.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "component.h"
+
+
+void menu(){
+    printf("\n");
+    printf("\n");
+    printf("                   Escolha uma das opcoes                          \n");
+    printf("\n");
+    printf("\n");
+    printf("  [ 1 ] - executar a ordenacao do arquivo usando Bubble Sort         \n");
+    printf("  [ 2 ] - executar a ordenacao do arquivo usando Insertion Sort      \n");
+    printf("  [ 3 ] - executar a ordenacao do arquivo usando Selection Sort      \n");
+    printf("  [ 4 ] - executar a ordenacao do arquivo usando Shell Sort          \n");
+    printf("  [ 5 ] - executar a ordenacao do arquivo usando Quicksort(Lomuto)   \n");
+    printf("  [ 6 ] - executar a ordenacao do arquivo usando Mergesort           \n");
+    printf("  [ 7 ] - executar a ordenacao do arquivo usando Radixsort           \n");
+    printf("  [ 8 ] - executar a ordenacao do arquivo usando QuickSort(Hoare)    \n");
+    printf("\n");
+    printf("\n");
+    printf(" > ");
+}
